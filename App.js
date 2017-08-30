8082129051b4ecf65a1f5242998d7ad81cf87466
Jie Wei <email2jie@gmail.com>
2017-08-29 20:31:25 -0700

    configured logout screen and functionality

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/components/common';
+import { CardSection, Header, Button, Spinner } from './src/components/common';
 import LoginForm from './src/components/LoginForm.js';
 
 class App extends Component {
+
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp( 
       {
@@ -15,19 +18,53 @@ class App extends Component {
         storageBucket: "rn-auth-774ad.appspot.com",
         messagingSenderId: "855962926595"
       });
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({loggedIn: true});
+      }else {
+        this.setState({loggedIn: false});
+      }
+
+    })
   }
 
-  render() {
-    return (
-      <View>
-        <Header headerText="Authentication">
-        </Header>
-        <LoginForm />
-      </View>	
-    )
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>Log Out</Button>
+          </CardSection>
+          );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={styles.spinnerStyle}>
+            <Spinner />
+          </View>
+          );
+
   }
+}
+
+render() {
+  return (
+    <View>
+      <Header headerText="Authentication">
+      </Header>
+        {this.renderContent()}
+    </View>	
+  )
+}
 
 
 }
+
+const styles = {
+  spinnerStyle: {
+    paddingTop: 50
+  }
+};
 
 export default App;
